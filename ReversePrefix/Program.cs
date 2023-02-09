8575488603f1e74d1912b859f094dc1a0b260cd2
Дмитрий Грабовский <dimokurl@gmail.com>
2023-02-09 12:30:33 +0300

    Добавьте файлы проекта.

---
+++ ReversePrefix/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ReversePrefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "abcd";
+
+            char ch = 'z';
+
+            Console.WriteLine(ReversePrefix(word, ch));
+        }
+
+        public static string ReversePrefix(string word, char ch)
+        {
+            System.Text.StringBuilder sb = new();
+            for(int i = word.IndexOf(ch); i >= 0; i--)
+            {
+                sb.Append(word[i]);
+            }
+            for (int i = word.IndexOf(ch) + 1; i < word.Length; i++)
+            {
+                sb.Append(word[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
